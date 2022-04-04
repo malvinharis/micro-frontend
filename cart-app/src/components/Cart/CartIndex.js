493c03a2df2b66cec442f3251f1aa1b792a433a6
malvinharis <alvinharis08@gmail.com>
2022-04-04 10:31:46 +0700

    chore: handle remove product function & receive post message

--- cart-app/src/components/Cart/CartIndex.js
+++ cart-app/src/components/Cart/CartIndex.js
@@ -1,8 +1,74 @@
 import React from 'react';
+import { useEffect, useState } from "react";
 
 const CartIndex = () => {
+  const [product, setProduct] = useState({});
+  const [productList, setProductList] = useState([]);
+  const [isCartShow, setIsCartShow] = useState(false);
+
+  function openModal () {
+    window.addEventListener("message", function (v) {
+      let data = v.data
+      if(data.isCartShow){
+        return setIsCartShow(data.isCartShow)       
+      }
+      if(data.product){
+        const newProduct = {...data.product, qty: 1};
+        setProduct(newProduct)
+        setProductList(item => {
+          if(item.length === 0){
+            return [...item, newProduct]
+          } else{
+            const checkDuplicate = item.find(itemChild => 
+              itemChild.title === newProduct.title  
+            )
+            if(checkDuplicate){
+              item.forEach(
+                itemChild2 =>
+                {
+                  if(itemChild2.title === newProduct.title){
+                    itemChild2.qty++
+                  }
+                }
+              )
+              return [...item]
+            } else{
+              return [...item, newProduct]
+            }
+          }
+        })
+      }
+    })
+  }
+  
+  function closeModal(){
+    setIsCartShow(false)
+    if(!isCartShow) window.parent.postMessage({"isCartShow": isCartShow});
+  }
+  
+  const removeProduct = (e, product) =>{
+    e.preventDefault();
+    if (window && window.parent) {
+      setProductList(item => {
+        return item.filter( itemChild => itemChild.title !== product.title)
+      })
+      window.parent.postMessage({removeProduct : product}, '*');
+    }
+  }
+
+  function calculateGrandTotal(products){
+    const grandTotal = products.reduce((total, currentValue) => total = total + (currentValue.price * currentValue.qty),0);
+    return grandTotal
+  }
+
+  useEffect(() => {
+    openModal();
+  }, []);
+
   return (
-    <div className="modal">
+    <div className={
+      isCartShow ? "modal modal-open" : "modal"
+    }>
       <div className="modal-overlay">
         <div className="modal-wrapper">
           <div className="cart">
@@ -10,82 +76,69 @@ const CartIndex = () => {
               <div className="cart__title">
                 Shopping Cart
               </div>
-              <button className="main__button main__button-icon">
-                <i class="fa-solid fa-xmark"></i>
+              <button className="main__button main__button-icon" onClick={closeModal}>
+                <i className="fa-solid fa-xmark"></i>
               </button>
             </div>
             <div className="cart__body"> 
               <div className="cart__content">
-                <div className="cart__list">
-                  <div className="cart__item">
-                    <div className="cart__item-body"> 
-                      <div class="cart__item-content">
-                        <div class="cart__item-image">
-                          <img src="http://images.thenorthface.com/is/image/TheNorthFace/236x204_CLR/mens-better-than-naked-jacket-AVMH_LC9_hero.png" />
-                        </div>
-                        <div className="cart__item-group">
-                          <div class="cart__item-title">
-                            Product A
+                {productList.length > 0 && (
+                  <div className="cart__list">
+                    {/* Product List in Cart */}
+                    {productList.map((item) => (
+                      <div className="cart__item" key={item.uuid}>
+                        <div className="cart__item-body"> 
+                          <div className="cart__item-content">
+                            <div className="cart__item-image">
+                              <img src={item.image} />
+                            </div>
+                            <div className="cart__item-group">
+                              <div className="cart__item-title">
+                                {item.title}
+                              </div>
+                              <div className="cart__item-qty">
+                                Quantity: {item.qty}
+                              </div>
+                            </div>
                           </div>
-                          <div class="cart__item-qty">
-                            Quantity: 2
+                          <div className="cart__item-content">
+                            <div className="cart__item-price">
+                              $ {item.price}
+                            </div>
+                            <cart className="cart__item-remove" onClick={(e) => removeProduct(e, item)}>
+                              <i className="fa-solid fa-trash"></i>
+                            </cart>
                           </div>
                         </div>
                       </div>
-                      <div class="cart__item-content">
-                        <div class="cart__item-price">
-                          $ 100
-                        </div>
-                        <cart className="cart__item-remove">
-                          <i class="fa-solid fa-trash"></i>
-                        </cart>
-                      </div>
-                    </div>
+                    ))}
                   </div>
-
-                  <div className="cart__item">
-                    <div className="cart__item-body"> 
-                      <div class="cart__item-content">
-                        <div class="cart__item-image">
-                          <img src="http://images.thenorthface.com/is/image/TheNorthFace/236x204_CLR/mens-better-than-naked-jacket-AVMH_LC9_hero.png" />
-                        </div>
-                        <div className="cart__item-group">
-                          <div class="cart__item-title">
-                            Product B
-                          </div>
-                          <div class="cart__item-qty">
-                            Quantity: 2
-                          </div>
-                        </div>
-                      </div>
-                      <div class="cart__item-content">
-                        <div class="cart__item-price">
-                          $ 100
-                        </div>
-                        <cart className="cart__item-remove">
-                          <i class="fa-solid fa-trash"></i>
-                        </cart>
-                      </div>
-                    </div>
+                )}
+                {productList.length === 0 && (
+                  <div className='cart__empty'>
+                    Your Cart is Empty
                   </div>
-                </div>
+                )}
               </div>
+              {productList.length > 0 && (
               <div className="cart__group">
                 <div className="cart__information">
                   <div className="cart__information-title">
                     Grand Total
                   </div>
                   <div className="cart__information-value">
-                    $ 650
+                    $ {calculateGrandTotal(productList)}
                   </div>
                 </div>
               </div>
+              )}
             </div>
           </div>
         </div>
       </div>
     </div>
   );
+
 }
 
 export default CartIndex;
