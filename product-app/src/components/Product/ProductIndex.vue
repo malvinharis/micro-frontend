407accede12ecbc043dc378901f5970861e500ef
malvinharis <alvinharis08@gmail.com>
2022-04-04 10:30:51 +0700

    chore: handle add to cart function & receive post message

--- product-app/src/components/Product/ProductIndex.vue
+++ product-app/src/components/Product/ProductIndex.vue
@@ -2,7 +2,7 @@
   <div class="products">
     <div class="products-wrapper">
       <div class="product__list" >
-        <div class="product" v-for="(product, idx) in products" :key="idx">
+        <div v-for="(product, idx) in products" :key="idx" :class="product.stock > 0 ? 'product' : 'product product__out-of-stock'">
           <div class="product__head">
             <div class="product__image">
                <img :src="product.image" />
@@ -17,15 +17,15 @@
                 {{ product.description }}
               </div>
               <div class="product__qty">
-                Stock: {{ product.qty }}
+                {{ product.stock > 0 ? `Stock: ${product.stock}` : `Out of Stock` }}
               </div>
             </div>
             <div class="product__group">
               <div class="product__price">
                 $ {{ formatPrice(product.price) }}
               </div>
-              <button class="main__button">
-                Add to Cart
+              <button class="main__button" @click="handleAddToCart(product)">
+                {{ product.stock > 0 ? 'Add to Cart' : 'Out of Stock' }}
               </button>
             </div>
           </div>
@@ -40,11 +40,41 @@ export default {
   data: () => ({
     products: data.products
   }),
+  mounted () {
+    window.addEventListener('message', this.receiveMessage)
+  },
+  beforeDestroy () {
+    window.removeEventListener('message', this.receiveMessage)
+  },
   methods: {
+    receiveMessage(v){
+      let data = v.data
+      if(data.removeProduct){  
+        this.products.map( item => {
+          const check = item.id === data.removeProduct.id
+          if(check){
+            item.stock = item.stock + data.removeProduct.qty
+          }
+          return item
+        })
+      }
+    },
     formatPrice(value) {
       const val = (value / 1).toFixed(0).replace('.', ',')
       return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.')
     },
+    handleAddToCart(v) {
+				let data = JSON.parse(JSON.stringify(v));
+        let qty = [v].length
+        this.products.forEach(item => {
+            if(item.title === v.title) {
+              if(qty <= item.stock){
+                item.stock = item.stock - qty
+				        window.parent.postMessage({"product": data}, 'http://localhost:8080/');
+              }
+            }
+        })
+		}
   },
 }
 </script>
